597c22a112665f064e8cab985b44a8a086b2f88b
G4rrow <gp.roaq@gmail.com>
2022-09-06 23:57:28 -0300

    Branch 0.1
    Validacion agregada

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,3 +1,5 @@
+import { AlertController } from '@ionic/angular';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formularioLogin: FormGroup = this.fb.group({
+    "username": ['', [Validators.required]],
+    "password": ['', [Validators.required, Validators.minLength(5),Validators.maxLength(20)]]
+
+  });
+
+  constructor(  public fb: FormBuilder,
+                public alertController: AlertController) { }
 
   ngOnInit() {
   }
+
+  async ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario = JSON.parse(localStorage.getItem('usuario'))
+
+    if(usuario.correo == f.correo && usuario.password == f.password){
+      console.log('Ingresado')    
+    }else{      const alert = await this.alertController.create({
+      header: 'Datos incorrectos',
+      message: 'Datos ingresados incorrectos',
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
+      
+
+    }
+  
+    }
+
 
 }
